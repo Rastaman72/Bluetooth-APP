52456ff7eed70ac3f362be1d44854dd214095fc9
Rastaman72 <m.niemiec@live.com>
2014-07-02 16:46:03 +0200

    Test commit

---
+++ BT/BT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BT
+//
+//  Created by LGBS dev on 7/2/14.
+//  Copyright (c) 2014 LGBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BT/BT/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BT
+//
+//  Created by LGBS dev on 7/2/14.
+//  Copyright (c) 2014 LGBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
